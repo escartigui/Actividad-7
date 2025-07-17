a1cec0c62e97cf52b7cadf278d5dbf7742ea8e2a
escartigui <escarlethmayali@gmail.com>
2025-07-16 19:20:58 -0600

    Escar

--- Estudiantes uni.py
+++ Estudiantes uni.py
@@ -1,5 +1,4 @@
 estudiantes = {}
-cantidad = int(input("Cuantos estudiante desea ingresar"))
 def menu():
     while True:
         print("\nBienvenido a la universidad")
@@ -11,12 +10,22 @@ def menu():
         op = input("Seleccione una opción: ")
 
         if op == "1":
+         cantidad = int(input("Cuantos estudiante desea ingresar"))
          for i in range(cantidad):
              print(f"Estudiante #{i+1}")
              carnet = input("Ingrese el numero de carnet: ")
              nombre = input("Ingrese el nombre: ")
              edad = input("Ingrese el edad: ")
              carrera = input("Ingrese el carrera: ")
-             cursos = input("Ingrese codigo del curso: ")
-
+             canticurs = int(input("cuantos cursos desea ingresar"))
+             for n in range(canticurs):
+              print(f"curso #{n+1}")
+              cursos = input("Ingrese el nombre de los cursos")
+             while True:
+              notatarea = input("ingrese la nota de su tarea: ")
+             if notatarea <= 0:
+                 print("verifica lo ingresado, no puede ser negativo")
+             else:
+                 break
+menu()
 
