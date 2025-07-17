656c1d3175d3f8e25ad083c02b5b6fa9e36717ef
escartigui <escarlethmayali@gmail.com>
2025-07-16 18:50:05 -0600

    Escar

---
+++ Estudiantes uni.py
@@ -0,0 +1,8 @@
+def menu():
+    while True:
+        print("\nBienvenido a la universidad")
+        print("MENU")
+        print("1.Registrar Estudiantes")
+        print("2.Mostrar los estudiantes y sus cursos")
+
+
